324eb65c6763524a38352321c643bbee50e465c7
Rajinikanthgatti <rajnikanthgatti97@gmail.com>
2021-11-07 20:05:39 +0530

    warning

--- src/components/pageComponents/ErrorPage.js
+++ src/components/pageComponents/ErrorPage.js
@@ -6,7 +6,7 @@ import HeaderPageBanner from '../HeaderPageBanner'
 export default function ErrorPage() {
     return (
         <HeaderPage>
-            <HeaderPageBanner title="404" subtitle="Page Not Found">
+            <HeaderPageBanner title="404 - ERROR" subtitle="Page Not Found">
                 <Link to="/" className="btn-primary">RETURN HOME</Link>
             </HeaderPageBanner>
         </HeaderPage>
